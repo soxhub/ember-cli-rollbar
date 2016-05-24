a3554e0177adb21c68231b1e2ac6fae7c74bedca
Rajiv Makhijani <rajiv@soxhub.com>
2016-05-24 15:12:52 -0700

    Include rollbar script only if 'enabled'

--- index.js
+++ index.js
@@ -11,11 +11,9 @@ module.exports = {
   contentFor: function(type, config) {
     var environment = this.app.env;
     config = config.rollbar || {};
-    var isProductionEnv = ['development', 'test'].indexOf(environment) === -1;
-    var includeScript = isProductionEnv || config.enabled;
+    var includeScript = config.enabled;
     if (type === 'head' && includeScript) {
       var rollbarConfig = merge({
-        enabled: isProductionEnv,
         captureUncaught: true,
         payload: {
           environment: environment
